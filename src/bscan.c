d36f4fc0f0dc6b9fd93fe010555e2472d0173fc8
徐东 <xud@knownsec.com>
2016-05-16 13:37:16 +0800

    add some code

--- src/bscan.c
+++ src/bscan.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <curl/multi.h>
+
+
+#define MAX 10
 
 int main(int argc, char *argv[]) 
 {
+  CURLM *cm;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+  cm = curl_multi_init();
+
+  curl_multi_setopt(cm, CURLMOPT_MAXCONNECTS, (long)MAX);
+
+  curl_multi_cleanup(cm);
+  curl_global_cleanup();
+
   printf("good\n");
   return 0;
 }
