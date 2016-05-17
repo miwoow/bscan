912c3aecf15dbb2c60b704f21c30a4e145204020
xud <xdsecret1@gmail.com>
2016-05-17 11:05:25 +0800

    finish one get method

--- src/bscan.c
+++ src/bscan.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <curl/multi.h>
+#include <pthread.h>
+#include <curl/curl.h>
 
 
-#define MAX 10
+size_t resp_write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) 
+{
+  FILE *fh = (FILE*)userdata;
+  size_t num = 0;
+  if (fh) {
+    num = fwrite(ptr, size, nmemb, fh);
+  }
+  return num;
+}
+
 
 int main(int argc, char *argv[]) 
 {
-  CURLM *cm;
 
+  CURL *eh;
+  CURLcode res;
+  FILE *fh;
   curl_global_init(CURL_GLOBAL_ALL);
-  cm = curl_multi_init();
-
-  curl_multi_setopt(cm, CURLMOPT_MAXCONNECTS, (long)MAX);
-
-  curl_multi_cleanup(cm);
+  
+  eh = curl_easy_init();
+  if (eh) {
+    curl_easy_setopt(eh, CURLOPT_URL, "http://www.baidu.com/");
+    fh = fopen("www.baidu.com", "w+");
+    curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, resp_write_callback);
+    curl_easy_setopt(eh, CURLOPT_WRITEDATA, fh);
+    res = curl_easy_perform(eh);
+    if (res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+	      curl_easy_strerror(res));
+    }
+    fclose(fh);
+    curl_easy_cleanup(eh);
+  }
   curl_global_cleanup();
-
   printf("good\n");
   return 0;
 }
